8e49379f83d6854abec8c565157221a8dc5d96c4
EGC-jospearui <jospearui@alum.us.es>
2021-02-12 10:06:02 +0000

    Añado Procfile y ajustes para despliegue en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -164,6 +164,10 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+BASEURL= "https://egc1230jospearui.herokuapp.com"
+
+APIS = {}
+
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
@@ -183,3 +187,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())

75accacecd85720815615e1fa00d17c2a62d2799
EGC-jospearui <jospearui@alum.us.es>
2021-02-12 10:26:57 +0000

    Modifico archivos para despliegue de docker

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,6 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
@@ -27,16 +28,16 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://10.5.0.1:8000'
+BASEURL = 'http://10.5.0.1:8009'
 
 APIS = {
-    'authentication': 'http://10.5.0.1:8000',
-    'base': 'http://10.5.0.1:8000',
-    'booth': 'http://10.5.0.1:8000',
-    'census': 'http://10.5.0.1:8000',
-    'mixnet': 'http://10.5.0.1:8000',
-    'postproc': 'http://10.5.0.1:8000',
-    'store': 'http://10.5.0.1:8000',
-    'visualizer': 'http://10.5.0.1:8000',
-    'voting': 'http://10.5.0.1:8000',
+    'authentication': 'http://10.5.0.1:8009',
+    'base': 'http://10.5.0.1:8009',
+    'booth': 'http://10.5.0.1:8009',
+    'census': 'http://10.5.0.1:8009',
+    'mixnet': 'http://10.5.0.1:8009',
+    'postproc': 'http://10.5.0.1:8009',
+    'store': 'http://10.5.0.1:8009',
+    'visualizer': 'http://10.5.0.1:8009',
+    'voting': 'http://10.5.0.1:8009',
 }
